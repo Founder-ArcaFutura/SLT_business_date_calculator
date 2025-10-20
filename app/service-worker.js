31b0e3c1d4c3a6ac9e427e4d1a8821f476fe39e9
Founder-ArcaFutura <founder@arcafutura.com>
2025-10-20 18:44:06 -0400

    Use relative asset URLs for web build

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,7 +1,14 @@
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open("slt-cache").then(cache => {
-      return cache.addAll(["/", "/index.html", "/manifest.json"]);
+      const scopeUrl = self.registration && self.registration.scope ? self.registration.scope : self.location.href;
+      const baseUrl = new URL("./", scopeUrl);
+      const assets = [
+        baseUrl.toString(),
+        new URL("index.html", baseUrl).toString(),
+        new URL("manifest.json", baseUrl).toString()
+      ];
+      return cache.addAll(assets);
     })
   );
 });
